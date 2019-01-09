c6f291513fede9f3309562e54ca8ab563f753822
Stefano Mancone <stefanomancone915@gmail.com>
2019-01-09 12:33:02 +0100

    Clean

--- script.py
+++
@@ -1,7 +0,0 @@
-file = '/home/mango/Documents/unipd/Earthquakes/SouthCalifornia-1982-2011_Physics-of-Data.dat'
-
-data = np.genfromtxt(file, skip_header=0, skip_footer=1, names=True, dtype=None, delimiter=' ')
-
-header = ["id","id_prv","time","magnitude","x","y", "z"]
-
-
